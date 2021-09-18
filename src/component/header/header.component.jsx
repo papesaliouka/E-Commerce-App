3bb4709910f1769a3264b71ac39e716b90f7ece0
Pape Saliou Ka <77701432+papesaliouka@users.noreply.github.com>
2021-09-18 18:20:47 +0000

    removing contact page

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -21,9 +21,6 @@ const Header= ({currentUser, hidden, signOutStart }) => {
         <OptionLink to='/shop' >
           SHOP
         </OptionLink>
-        <OptionLink to='/contact' >
-          CONTACT
-        </OptionLink>
         {
           currentUser ?
           <OptionLink as= 'div' onClick= {signOutStart}>SIGN OUT</OptionLink>
